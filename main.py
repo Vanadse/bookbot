a4e75e816392b302df0ce78536cd4a0c8a43d595
Vanadse <legend_of_mana1@yahoo.com.ar>
2025-05-25 19:27:02 -0300

    added character counter function

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import number_of_words_in #requires a string as an argument
+from stats import number_of_characters_in # requires a string as an argument
 
 # Returns a given file as a string ---------------
 def get_book_text(path_to_book):
@@ -9,7 +10,9 @@ def get_book_text(path_to_book):
 # Main starts here -------------------------------
 def main():
     num_words = number_of_words_in(get_book_text("books/frankenstein.txt"))
+    num_characters = number_of_characters_in(get_book_text("books/frankenstein.txt"))
     print(f"{num_words} words found in the document")
+    print(num_characters)
 
 # Dont edit below here ---------------------------
 main()
--- stats.py
+++ stats.py
@@ -1,6 +1,16 @@
-# Returns the number of words in a given string --
+# Returns the number of words in a given string -----------------------------------
 def number_of_words_in(given_string):
     count = 0
     for word in given_string.split():
         count += 1
     return(count)
+
+# Returns the amount of each type of character, including spaces, in a given string
+def number_of_characters_in(given_string):
+    characters = {}
+    for character in given_string.lower():
+        if character not in characters:
+            characters[character] = 1
+        else:
+            characters[character] += 1
+    return characters

5e1e3288241fd926c48bf6c23939b53b6d3e0f35
Vanadse <legend_of_mana1@yahoo.com.ar>
2025-05-25 21:16:54 -0300

    Added a function to output the results nicely

--- main.py
+++ main.py
@@ -1,18 +1,29 @@
 from stats import number_of_words_in #requires a string as an argument
 from stats import number_of_characters_in # requires a string as an argument
+from stats import make_sorted_list_from # requires a dictionary as an argument
 
 # Returns a given file as a string ---------------
 def get_book_text(path_to_book):
     with open(path_to_book) as f:
         return(f.read())
-    
+
 
 # Main starts here -------------------------------
 def main():
-    num_words = number_of_words_in(get_book_text("books/frankenstein.txt"))
-    num_characters = number_of_characters_in(get_book_text("books/frankenstein.txt"))
-    print(f"{num_words} words found in the document")
-    print(num_characters)
-
+    book = "books/frankenstein.txt"
+    num_words = number_of_words_in(get_book_text(book))
+    num_characters = number_of_characters_in(get_book_text(book))
+    #print(f"{num_words} words found in the document")
+    #print(num_characters)
+    #print(make_sorted_list_from(num_characters))
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book}...")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for character in make_sorted_list_from(num_characters):
+        char = character["char"]
+        num = character["num"]
+        print(f"{char}: {num}")
+    print("============= END ===============")
 # Dont edit below here ---------------------------
 main()
--- stats.py
+++ stats.py
@@ -14,3 +14,16 @@ def number_of_characters_in(given_string):
         else:
             characters[character] += 1
     return characters
+
+# Returns the value of "num" from a given dictionary
+def sort_on(dict):
+    return dict["num"]
+
+# Returns a sorted list created from a given dictionary
+def make_sorted_list_from(given_dictionary):
+    sorted_list = []
+    for character in given_dictionary:
+        if character.isalpha():
+            sorted_list.append({"char": character, "num": given_dictionary[character]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
